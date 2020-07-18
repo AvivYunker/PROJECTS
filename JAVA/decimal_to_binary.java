43aef3d6f726786e42532ead160a621f36e33b56
Aviv <avivyunker@gmail.com>
2020-07-18 06:02:12 -0700

    18/7/2020

---
+++ JAVA/decimal_to_binary.java
@@ -0,0 +1,27 @@
+
+import java.util.Scanner;
+class decimal_to_binary {
+	public static void main (String [] args) {
+		Scanner SC = new Scanner(System.in);
+		int x;
+		int results;
+		System.out.println("User, enter a number: ");
+		x = SC.nextInt();
+		results = decimal_to_binary(x);
+		System.out.println(x + " --> " + results);
+	}
+	public static int decimal_to_binary (int num) {
+		int res = 0;
+		int level = 1;
+		while (num > 0) {
+			int tempI = (int)(num / 2);
+			float tempF = (float)(num / 2.0);
+			tempF = tempF - tempI;
+			tempF = (int)(tempF * 2);
+			res += tempF * level;
+			level *= 10;
+			num = tempI;
+		}
+	return res;
+	}
+}
