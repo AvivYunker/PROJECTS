8c80969313ec0c78d74b3cb4a4a0c9410a6eb35f
Aviv <avivyunker@gmail.com>
2020-07-19 11:43:00 -0700

    19/7/2020

---
+++ GO/code/multiply_two_integers.go
@@ -0,0 +1,15 @@
+package main
+import "fmt"
+
+func main() {
+	var num1 int
+	var num2 int
+	fmt.Println("User, enter first number: ")
+	fmt.Scan(&num1)
+	fmt.Println("User, enter second number: ")
+	fmt.Scan(&num2)
+	fmt.Print("The factor of ", num1)
+	fmt.Print(" and of ", num2)
+	fmt.Println(" is:", (num1*num2))
+}
+
