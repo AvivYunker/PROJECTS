9b4af3459dd663e9ba2dde165c722dda07eef247
Aviv <avivyunker@gmail.com>
2020-07-19 12:53:12 -0700

    19/7/2020

---
+++ GO/code/factorial.go
@@ -0,0 +1,15 @@
+package main
+import "fmt"
+
+func main () {
+	var num int
+	res := 1
+	fmt.Println("User, enter a number: ")
+	fmt.Scan(&num)
+	orig := num
+	for num > 0 {
+		res *= num
+		num -= 1
+	}
+	fmt.Printf("%d --> %d\n", orig, res)
+}
