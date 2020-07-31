092d0bd6e3d392ec744bf514969edb1c4ab2b014
Aviv <avivyunker@gmail.com>
2020-07-31 13:52:42 -0700

    31/7/2020

---
+++ C#/code/sum_two_integers.cs
@@ -0,0 +1,25 @@
+using System;
+
+namespace aviv_yunker {
+        class sum_two_integers {
+                public static void Main (string[] args) {
+			string x,y;
+			int num1, num2;
+                        Console.WriteLine("User, enter the first number: ");
+			x = Console.ReadLine();
+			Console.WriteLine("User, enter the second number: ");
+			y = Console.ReadLine();
+			num1 = int.Parse(x);
+			num2 = int.Parse(y);
+			Console.WriteLine("The sum is: " + (num1+num2));
+                        // Console.ReadKey();
+                }
+        }
+}
+
+
+
+
+
+
+
