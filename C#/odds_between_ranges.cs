9a011f39e22a1fc97e1832704d70ea2159be77c5
Aviv <avivyunker@gmail.com>
2020-08-01 01:49:52 -0700

    1/8/2020

---
+++ C#/odds_between_ranges.cs
@@ -0,0 +1,22 @@
+namespace aviv_yunker {
+	class odds_between_ranges {
+		public static void Main (string[] args) {
+			string x, y;
+			int lower, upper;
+			System.Console.WriteLine("User, enter the lower edge: ");
+			x = System.Console.ReadLine();
+			System.Console.WriteLine("User, enter the upper edge: ");
+			y = System.Console.ReadLine();
+			lower = int.Parse(x);
+			upper = int.Parse(y);
+			System.Console.WriteLine("");
+			while (lower <= upper) {
+				if (lower % 2 == 1) {
+					System.Console.WriteLine("{0}", lower);
+				}
+				lower++;
+			}
+
+		}
+	}
+}
